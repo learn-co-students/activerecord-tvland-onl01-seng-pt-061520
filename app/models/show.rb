5c0e709f5568eea51a1d9363576595317079966a
kowalix-pl <kowalix007@gmail.com>
2020-08-27 21:20:15 +0100

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+      actors = self.actors
+      actors = actors.collect do |k,v| 
+      "#{k.first_name} #{k.last_name}"
+    end
+  end 
+
 end

be44eace9890dfd991622a000e03f848cf056901
mjaber <abujaber44@gmail.com>
2020-09-01 21:37:29 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+    belongs_to :network 
+    has_many :characters 
+
+    #attr_accessor :name, :day, :season, :genre
+
+    def actors_list
+        self.characters.collect do |character|
+            "#{character.actor.first_name} #{character.actor.last_name} - #{character.show.name}"
+        end.join
+    end 
   
 end

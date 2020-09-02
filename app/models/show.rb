fee54a79b9cf30ff1437576574ff766d4a0b0835
mjaber <abujaber44@gmail.com>
2020-09-01 21:39:24 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,12 +2,12 @@ class Show < ActiveRecord::Base
     belongs_to :network 
     has_many :characters 
 
-    #attr_accessor :name, :day, :season, :genre
+
 
     def actors_list
-        self.characters.collect do |character|
-            "#{character.actor.first_name} #{character.actor.last_name} - #{character.show.name}"
-        end.join
+        self.characters.map do |character|
+            "#{character.actor.first_name} #{character.actor.last_name}"
+        end
     end 
   
 end
